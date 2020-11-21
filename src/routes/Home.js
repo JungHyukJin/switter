a015eda902e5402a03f1474ebb77036cc27cec09
JungHyukJin <hyukjin4976@naver.com>
2020-11-21 15:06:53 +0900

    form and database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  <div>
-    <form>
-      <input></input>
-    </form>
-  </div>;
+  const [switt, setSwitt] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSwitt(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={switt}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Sweeter" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
