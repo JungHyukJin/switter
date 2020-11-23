d5aaab717a3fd53367a7ae56e540c63610d9c1ee
JungHyukJin <hyukjin4976@naver.com>
2020-11-23 20:21:35 +0900

    JS: CRUD done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import { dbService } from 'fbase';
+import React, { useEffect, useState } from 'react';
+import Switt from './Switt';
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [switt, setSwitt] = useState('');
-  const onSubmit = (event) => {
+  const [switts, setSwitts] = useState([]);
+
+  useEffect(() => {
+    dbService.collection('switts').onSnapshot((snapshot) => {
+      const swittArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setSwitts(swittArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await dbService.collection('switts').add({
+      text: switt,
+      createdAt: Date.now(),
+      createrId: userObj.uid,
+    });
+    setSwitt('');
   };
   const onChange = (event) => {
     const {
@@ -11,20 +31,31 @@ const Home = () => {
     } = event;
     setSwitt(value);
   };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={switt}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type="submit" value="Sweeter" />
-      </form>
-    </div>
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={switt}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120}
+            autoFocus
+          />
+          <input type="submit" value="Switt" />
+        </form>
+        <div>
+          {switts.map((switt) => (
+            <Switt
+              key={switt.id}
+              swittObj={switt}
+              isOwner={switt.createrId === userObj.uid}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/routes/Switt.js
@@ -0,0 +1,63 @@
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
+
+const Switt = ({ swittObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newSwitt, setNewSwitt] = useState(swittObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this switt?');
+    // console.log(ok);
+    if (ok) {
+      await dbService.doc(`switts/${swittObj.id}`).delete();
+    }
+  };
+  const onToggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`switts/${swittObj.id}`).update({
+      text: newSwitt,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewSwitt(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your switt"
+              value={newSwitt}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update" />
+          </form>
+          <button onClick={onToggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <button>
+          <h4>{swittObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onToggleEditing}>Edit Switt</button>
+              <button onClick={onDeleteClick}>Delete Switt</button>
+            </>
+          )}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Switt;
