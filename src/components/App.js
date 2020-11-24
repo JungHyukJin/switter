484d8a066a9aea2d7a020e65918fbf22bd2f0e96
JungHyukJin <hyukjin4976@naver.com>
2020-11-24 20:08:20 +0900

    JS: update profile bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from './Router';
 import { authService } from 'fbase';
-import fbase from 'fbase';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
-        setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
 
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         'Initializing...'
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,30 +1,44 @@
-import { authService, dbService } from 'fbase';
-import React, { useEffect } from 'react';
+import { authService } from 'fbase';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
     // history를 사용하거나, Router.js에서 Redirect를 사용가능.
   };
 
-  const getMySwitts = async () => {
-    const switts = await dbService
-      .collection('switts')
-      .where('createrId', '==', userObj.uid)
-      .orderBy('createdAt')
-      .get();
-    console.log(switts.docs.map((doc) => doc.data()));
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
   };
 
-  useEffect(() => {
-    getMySwitts();
-  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
